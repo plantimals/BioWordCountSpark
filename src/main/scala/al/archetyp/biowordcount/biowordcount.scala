7120e9ac33780c11983bf2cd61c875e94a4263de
Rob Long <rob@stubb.local>
2014-09-07 22:59:12 -0500

    first arg is arg 0, remote .txt from output, as it is a directory

--- src/main/scala/al/archetyp/biowordcount/biowordcount.scala
+++ src/main/scala/al/archetyp/biowordcount/biowordcount.scala
@@ -5,10 +5,10 @@ import org.apache.spark.SparkConf
 object BioWordCountSpark {
   def main(args:Array[String]){
     val sc = new SparkContext(new SparkConf().setAppName("BioWordCount-Spark"))
-    val file = sc.textFile(args(1))
+    val file = sc.textFile(args(0))
     val rows = file.filter(!_.startsWith("#"))
     val refVar = rows.map(_.split("\t").drop(3).take(2))
     val answer = refVar.map(data => (data(0)+" -> "+data(1) -> 1)).reduceByKey(_ + _)
-    answer.sortByKey().saveAsTextFile("answer.txt")
+    answer.sortByKey().saveAsTextFile("answer")
   }
 }
